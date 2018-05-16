9fdfa2d543360662fcb055dc2401e02b1020d74a
vincent732 <vincent732@gmail.com>
2018-05-16 09:02:57 +0800

    NonDecreasingArray ReverseInteger RotateArray

--- ReverseInteger.java
+++ ReverseInteger.java
@@ -1,33 +1,66 @@
-
 public class ReverseInteger {
+    /*
+    * Given an array, rotate the array to the right by k steps, where k is non-negative.
+    *Example 1:
 
-    public int reverse(int x) {
-        boolean reverseSign = false;
-        if (x < 0) {
-            reverseSign = true;
+    Input: [1,2,3,4,5,6,7] and k = 3
+    Output: [5,6,7,1,2,3,4]
+    Explanation:
+    rotate 1 steps to the right: [7,1,2,3,4,5,6]
+    rotate 2 steps to the right: [6,7,1,2,3,4,5]
+    rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+    Example 2:
+
+    Input: [-1,-100,3,99] and k = 2
+    Output: [3,99,-1,-100]
+    Explanation: 
+    rotate 1 steps to the right: [99,-1,-100,3]
+    rotate 2 steps to the right: [3,99,-1,-100]
+
+    */
+    public void rotate(int[] nums, int k) {
+        if (nums.length == 1) {
+            return;
         }
-        x = Math.abs(x);
-        int numOfDigit = (int)(Math.log10(x)+1);
-        int multiplyer = 10;
-        int output = 0;
-        while(x > 0) {
-            int reminder = x % multiplyer;
-            x = x / multiplyer;
-            output += reminder * Math.pow(10, numOfDigit - 1);
-            numOfDigit--;
-            if (output == Integer.MAX_VALUE || output == Integer.MIN_VALUE) {
-                output = 0;
-                break;
+        int length = nums.length;
+        int[] temp = new int[k = k > length ? k - length:k];
+        int start_index = Math.abs(length -k);
+        // store shifted element
+        for (int i = 0; i < temp.length; i++) {
+            temp[i] = nums[start_index];
+            start_index++;
+        }
+
+        // shift them
+        for (int i = length - k - 1; i >= 0; i--) {
+            nums[i + k] = nums[i];
+        }
+        // fill the empty element
+        for (int i = 0; i < temp.length; i++) {
+            nums[i] = temp[i];
+        }
+    }
+
+    public void rotate_2(int[] nums, int k) {
+        while (k > 0) {
+            int temp = nums[0];
+            for (int i = 1;i < nums.length; i++) {
+                int current = nums[i];
+                nums[i] = temp;
+                temp = current;
             }
+            nums[0] = temp;
+            k--;
         }
-        output = reverseSign ? 0 - output : output;
-        return output;
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
     }
 
     public static void main(String[] args) {
-        ReverseInteger a = new ReverseInteger();
-        System.out.println(Integer.MAX_VALUE);
-        System.out.println(Integer.MIN_VALUE);
-        System.out.print(a.reverse(1534236469));
+        ReverseInteger ri = new ReverseInteger();
+        int[] input_array = new int[]{1,2,3,4,5,6,7};
+        ri.rotate_2(input_array, 3);
     }
 }
