1f9de210d51b467ffa1de09f9e376845d47f9898
vincent732 <vincent732@gmail.com>
2018-05-30 07:43:31 +0800

    complete some problems

--- String/LongestPalindromicSubstring.java
+++ String/LongestPalindromicSubstring.java
@@ -15,65 +15,49 @@ import java.util.Arrays;
 
  */
 class LongestPalindromicSubstring {
+
     public String longestPalindrome(String s) {
-        int i = 0, j = s.length() - 1;
-        int[][] dp = new int[1000][1000];
-        for (int[] arr : dp) {
-            Arrays.fill(arr, -1);
+        if (s.length() <= 0) {
+            return s;
         }
-        int[][] record = new int[1000][1000];
-        int maxLength = longestPalindrome(s, i, j, dp, record);
-        System.out.println("Max Length:" + maxLength);
-        return print(s, i, j, record);
-    }
+        int n = s.length();
+        boolean[][] table = new boolean[n][n];
+        for (boolean[] t : table) {
+            Arrays.fill(t, false);
+        }
+        int maxLength = 0;
+        int start = 0;
+        for (int i = 0;i < n; i++) {
+            table[i][i] = true;
+            maxLength = 1;
+        }
 
-    private String print(String s, int i, int j, int[][] record) {
-        if (i > j) {
-            return "";
+        for (int i = 0;i < n - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                table[i][i+1] = true;
+                start = i;
+                maxLength = 2; 
+            }
         }
-        if (i == j) {
-            return s.substring(i,i+1);
-        }
-        String result = "";
-        if (record[i][j] == 0) {
-            result = s.substring(i, i+1) + print(s, i + 1, j - 1, record) + s.substring(i, i + 1);
-        } else if (record[i][j] == 1) {
-            result = print(s, i + 1, j, record);
-        } else {
-            result = print(s, i, j - 1, record);
-        }
-        return result;
-    }
 
-    private int longestPalindrome(String s, int i, int j, int[][] dp, int[][] record) {
-        if (i == j) {
-            return 1;
-        } 
+        for (int k = 3; k <= n; k++) {
+            for (int i = 0;i < n - k + 1; i++) {
+                int j = i + k - 1;
+                if (table[i + 1][j - 1] && s.charAt(i) == s.charAt(j)) {
+                    table[i][j] = true;
 
-        if (i > j) {
-            return 0;
-        } 
-        if (dp[i][j] != -1) {
-            return dp[i][j];
+                    if (k > maxLength) {
+                        maxLength = k;
+                        start = i;
+                    }
+                }
+            }
         }
-        if (s.charAt(i) == s.charAt(j)) {
-            dp[i][j] = longestPalindrome(s, i + 1, j - 1, dp, record) + 2;
-            record[i][j] = 0;
-        } else if (longestPalindrome(s, i + 1, j, dp, record) > longestPalindrome(s, i, j - 1, dp, record)){
-            dp[i][j] = longestPalindrome(s, i + 1, j, dp, record);
-            record[i][j] = 1;
-        } else if (longestPalindrome(s, i + 1, j, dp, record) < longestPalindrome(s, i, j - 1, dp, record)) {
-            dp[i][j] = longestPalindrome(s, i, j - 1, dp, record);
-            record[i][j] = 2;
-        } else {
-            dp[i][j] = longestPalindrome(s, i, j - 1, dp, record);
-            record[i][j] = 3;
-        }
-        return dp[i][j];
+        return s.substring(start, start + maxLength);
     }
 
     public static void main(String[] args) {
         LongestPalindromicSubstring lps = new LongestPalindromicSubstring();
-        System.out.println(lps.longestPalindrome("abcda"));
+        System.out.println(lps.longestPalindrome("babad"));
     }
 }
